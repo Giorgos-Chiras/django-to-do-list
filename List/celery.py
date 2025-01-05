dc9ce951ffb11cec8ac9ae007c29cae27a2b3283
Giorgos-Chiras <166930497+Giorgos-Chiras@users.noreply.github.com>
2025-01-05 17:30:31 +0200

    Remove __pycache__ from repository

--- List/celery.py
+++ List/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set default Django settings module for 'celery'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'List.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to-do-list.settings')
 
 app = Celery('your_project')
 
