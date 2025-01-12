a83942a684a82697fda6fb8f73e982105e290fe5
Giorgos-Chiras <166930497+Giorgos-Chiras@users.noreply.github.com>
2025-01-12 14:18:26 +0200

    Changeds URL

--- List/settings.py
+++ List/settings.py
@@ -35,7 +35,7 @@ CACHES = {
 }
 
 ALLOWED_HOSTS = ['django-to-do-list.up.railway.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://django-to-do-list-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://django-to-do-list.up.railway.app']
 
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
 
